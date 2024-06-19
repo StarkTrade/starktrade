5494ee0e549d955d207633a32ec9b4fe932f7a4c
Adebara123 <adebaraayomide@gmail.com>
2024-06-19 10:32:36 +0100

    Chore: made buy and sell dynamic using helper

---
+++ src/trade/helper.mjs
@@ -0,0 +1,88 @@
+import api from "../services/api.mjs";
+import {STRK} from "../utils/constants.mjs"
+
+
+function getAccount (provider, accountAddress, privateKey) {
+    const account = new Account(provider, accountAddress, privateKey);
+    return account
+}
+
+
+async function getQuoteData (sellTokenAddress, buyTokenAddress, sellAmount, takerAddress ) {
+    const data = {
+        sellTokenAddress: sellTokenAddress,
+        buyTokenAddress: buyTokenAddress,
+        sellAmount: sellAmount,
+        takerAddress: takerAddress
+    }
+    try {
+      const response = await api.getQuote(data)
+      return response
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+async function buildMessageTypedData (quoteID, takerAddress, maxGasTokenAmount) {
+    const data = {
+            quoteId: quoteID,
+            takerAddress: takerAddress,
+            slippage: 0.05,
+            includeApprove: true,
+            gasTokenAddress: STRK,
+            maxGasTokenAmount: maxGasTokenAmount
+        }
+    try {
+        const response = await api.buildTypedData(data)
+        return response
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+async function getSignature (provider, accountAddress, privateKey,typedDataValidate) {
+    let account = getAccount(provider, accountAddress, privateKey);
+    const signature = await account.signMessage(typedDataValidate)
+    return signature
+
+}
+
+
+async function execute (privateKey, sellTokenAddress, buyTokenAddress, sellAmount, takerAddress, maxGasTokenAmount) {
+    let quoteID = await getQuoteData(sellTokenAddress, buyTokenAddress, sellAmount, takerAddress).quoteId;
+
+    let typedData = await buildMessageTypedData(quoteID, takerAddress, maxGasTokenAmount)
+
+    let sig = await getSignature(process.env.RPC_URL_TESTNET, takerAddress, privateKey, typedData)
+
+    let data = {
+        quoteId : quoteID,
+        signature: [sig]
+    }
+
+    try {
+        const response = await api.executeSwap(data)
+        return response
+    } catch (err) {
+        console.error(err);
+    }
+    
+}
+
+async function getSupportedTokens () {
+   try {
+    let response = await api.getSupportedTokens()
+    return response
+   } catch (err) {
+    console.error(err)
+   }
+
+}
+
+
+export {
+    execute,
+    getAccount,
+    getSupportedTokens
+}

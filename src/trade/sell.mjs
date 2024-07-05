50484370e7d225504c45190ba656a20b9696975c
casweeney <casweeno2000@gmail.com>
2024-07-05 02:36:21 +0100

    Sell structure implements

--- src/trade/sell.mjs
+++ src/trade/sell.mjs
@@ -1,22 +1,42 @@
 import {InlineKeyboard} from "grammy";
 import { tradeWithAvnu } from "../services/avnu/index.mjs";
 
-export async function sell_25() {
-
+export async function sell_25(accountAddress, privateKey, slippage, tokenInAddress, tokenOutAddress) {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
-export async function sell_50() {
-  
+export async function sell_50(accountAddress, privateKey, slippage, tokenInAddress, tokenOutAddress) {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
-export async function sell_75() {
-  
+export async function sell_75(accountAddress, privateKey, slippage, tokenInAddress, tokenOutAddress) {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
-export async function sell_100() {
-  
+export async function sell_100(accountAddress, privateKey, slippage, tokenInAddress, tokenOutAddress) {
+  try {
+    
+  } catch (error) {
+    
+  }
 }
 
-export async function sell_x() {
-  
+export async function sell_x(accountAddress, privateKey, slippage, sellAmount, tokenInAddress, tokenOutAddress) {
+  try {
+    
+  } catch (error) {
+    
+  }
 }

4c2843f46f515767da97c0a4aa22dc6264fd1500
casweeney <casweeno2000@gmail.com>
2024-07-08 01:01:33 +0100

    WIP: sell structure implemented

--- src/trade/sell.mjs
+++ src/trade/sell.mjs
@@ -1,2 +1,17 @@
 import { tradeWithAvnu } from "../services/avnu/index.mjs";
 
+export async function executeSell(accountAddress, privateKey, slippage, tokenInAddress, tokenOutAddress, buyPercent) {
+    try {
+  
+    } catch (error) {
+  
+    }
+}
+
+export async function sell_x(accountAddress, privateKey, slippage, sellAmount, tokenInAddress, tokenOutAddress) {
+    try {
+  
+    } catch (error) {
+  
+    }
+}
